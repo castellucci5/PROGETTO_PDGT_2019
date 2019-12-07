22976921bcfb97bf7f81db11ecad46690c64ca83
castellucci5 <giakispeed@live.it>
2019-12-07 19:58:45 +0100

    Add files via upload
    Creazione del file client.php per la verifica della API stampa_db_num_veicoli.php

---
+++ client.php
@@ -0,0 +1,50 @@
+<?php
+/* File contenente il codice sorgente del client */
+
+                               //includiamo file contenente le funzioni usate dal client
+require 'function.php';
+
+                               //stampa info programmatore e client
+echo "\n*********************************\n";
+echo "|  Progetto PDGT  A.A. 2019/2020   |\n";
+echo "|   Studente: Giacomo Castellucci  |\n";
+echo "|        Matricola: 279269         |\n";
+echo "|                                  |\n";
+echo "| Servizio client per collegamento |\n";
+echo "|    al database del numero di     |\n";
+echo "|     veicoli della regione        |\n";
+echo "|    FRIULI VENEZIA GIULIA         |\n";
+echo "------------------------------------\n\n";
+
+$close_client = 1;            //impostiamo variabile di controllo ciclo do-while del menù
+
+                              //entriamo nel menù
+do {
+  echo "\n\nSelezionare la richiesta da eseguire al database: \n";
+  echo "\t[1] Stampa completa del database.\n";
+  echo "\t[2] Chiusura del client.\n\n";
+  $first_ch = readline();     //acquisizione scelta dell'utente
+  $first_ch = intval($first_ch);
+  
+   if ($first_ch === 1) {
+    //inizializzazione richiesta HTTP tramite CURL
+    $handle = curl_init('http://giakispeed.altervista.org/PDGT/stampa_db_num_veicoli.php');
+    //richiesta della risposta HTTP come stringa
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    //esecuzione della richiesta HTTP
+    $response = curl_exec($handle);
+    //estrazione del codice di risposta (HTTP status)
+    $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+    //stampa ordinata delle info del database
+    stampa_numero_veicoli ($http_code,$response);
+    //TERMINE del codice eseguito per la prima scelta del menù
+  }
+   elseif ($first_ch === 2) {
+	$close_client = 0;         //impostando la variabile a 0 interrompiamo l'esecuzione del client
+    echo "\n\nTerminazione corretta del client, arrivederci !\n\n";
+    exit;                      //terminazione del programma  
+  }
+  } while ($close_client !== 0); 
+                              //chiusura della sessione CURL
+curl_close($handle);
+?>
